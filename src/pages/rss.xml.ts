a9ad44d7ccd1c6b95eef8d2a6701c88020278852
Franklin Shera <fshera96@gmail.com>
2024-02-01 06:15:35 +0300

    add  rss

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,23 @@
+import rss from "@astrojs/rss";
+import config from "../config/config.json";
+import { getCollection } from "astro:content";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async (context) => {
+  const posts = await getCollection("posts", ({ data }) => {
+    return data.draft !== true;
+  });
+  return rss({
+    title: config.site.title,
+    description: config.metadata.meta_description,
+    site: context.site ?? config.site.base_url,
+    items: posts.map((post) => ({
+      stylesheet: "/rss/styles.xsl",
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      categories: post.data.categories,
+      link: "/" + post.slug,
+    })),
+  });
+};
